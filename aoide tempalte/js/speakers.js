111b83563d629be2d815141229cc7a78201dc317
mertbabaokulmus <mertbaba.okulmus@hotmail.com>
2025-01-02 22:34:01 +0300

    feat: new banner

--- aoide tempalte/js/speakers.js
+++ aoide tempalte/js/speakers.js
@@ -1,41 +1,49 @@
 const speakers = [
     {
         name: "Ivan Kulis",
-        title: "chief economist",
-        company: "some company name, city",
+        title: "CEO",
+        company: "Albaraka Bank",
         description:
-            "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit.",
+            "",
         image: "img/t3.jpg",
         social: [
-            { platform: "Twitter", url: "https://twitter.com/ivan_kulis" },
-            { platform: "Facebook", url: "https://facebook.com/ivan.kulis" },
-            { platform: "Behance", url: "https://behance.net/ivankulis" },
+            { platform: "TW", url: "https://twitter.com/ivan_kulis" },
+            { platform: "Face", url: "https://facebook.com/ivan.kulis" },
         ],
     },
     {
         name: "Marco Furius",
-        title: "research director",
-        company: "some company name, city",
+        title: "CEO",
+        company: "Albaraka Bank",
         description:
-            "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit.",
+            "",
         image: "img/t4.jpg",
         social: [
-            { platform: "Twitter", url: "https://twitter.com/marco_furius" },
-            { platform: "Facebook", url: "https://facebook.com/marco.furius" },
-            { platform: "Behance", url: "https://behance.net/marcofurius" },
-            { platform: "LinkedIn", url: "https://linkedin.com/in/marcofurius" },
+            { platform: "TW", url: "https://twitter.com/marco_furius" },
+            { platform: "LkIn", url: "https://linkedin.com/in/marcofurius" },
         ],
     },
     {
         name: "Nia Johnson",
-        title: "head of innovation",
-        company: "some company name, city",
+        title: "CEO",
+        company: "Albaraka Bank",
         description:
-            "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit.",
+            "",
         image: "img/t5.jpg",
         social: [
-            { platform: "Behance", url: "https://behance.net/niajohnson" },
-            { platform: "LinkedIn", url: "https://linkedin.com/in/niajohnson" },
+            { platform: "LkIn", url: "https://linkedin.com/in/niajohnson" },
+        ],
+    },
+    {
+        name: "Ivan Kulis",
+        title: "CEO",
+        company: "Albaraka Bank",
+        description:
+            "",
+        image: "img/t3.jpg",
+        social: [
+            { platform: "TW", url: "https://twitter.com/ivan_kulis" },
+            { platform: "Face", url: "https://facebook.com/ivan.kulis" },
         ],
     },
 ];
@@ -50,19 +58,21 @@ speakers.map((speaker) => {
         )
         .join("");
     const speakerHTML = `
-    <div class="col-md-3">
-      <div class="speaker-image">
-        <h6>${speaker.name}</h6>
-        <img src="${speaker.image}" alt="${speaker.name}">
-      </div>
-    </div>
-    <div class="col-md-9 mt-4 mt-md-0 mb-4">
-      <h5>${speaker.title}</h5>
-      <p><em>${speaker.company}</em></p>
-      <p class="mt-4">${speaker.description}</p>
-      <div class="social-team mt-4">
-        ${socialLinks}
-      </div>
+    <div >
+        <div style="padding-left: 15px !important;" class="">
+            <div style="margin-bottom: 8px !important" class="speaker-image">
+                <img  src="${speaker.image}" alt="${speaker.name}">
+                <div>${speaker.name}</div>
+            </div>
+        </div>
+        <div  class="col-md-9 mt-4 mt-md-0 mb-4">
+            <h5>${speaker.title}</h5>
+            <p>${speaker.company}</p>
+            <p class="mt-4">${speaker.description}</p>
+            <div style="display: flex;" class="social-team mt-4">
+                ${socialLinks}
+            </div>
+        </div>
     </div>
   `;
     container.innerHTML += speakerHTML;
