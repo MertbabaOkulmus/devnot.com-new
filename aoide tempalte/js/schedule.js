0a8bcd548911373744a6dffd1526ced2dd6a3749
mertbabaokulmus <mertbaba.okulmus@hotmail.com>
2025-02-17 15:41:12 +0300

    feat: sunumlar

--- aoide tempalte/js/schedule.js
+++ aoide tempalte/js/schedule.js
@@ -3,15 +3,15 @@ document.addEventListener("DOMContentLoaded", function () {
         {
             salon: "Salon 1",
             events: [
-                { time: "15:00 pm - 17:00 pm", speaker: "Taylan Güney", image: "img/speakers/taylan-guney.jpg", title: "Keynote" },
-                { time: "18:00 pm - 19:00 pm", speaker: "Ahmet Yılmaz", image: "img/speakers/olcay-atlioglu.jpg", title: "Frontend Trends" }
+                { time: "15:00 pm", speaker: "Taylan Güney", image: "img/speakers/taylan-guney.jpg", title: "Keynote" },
+                { time: "18:00 pm", speaker: "Ahmet Yılmaz", image: "img/speakers/olcay-atlioglu.jpg", title: "Frontend Trends" }
             ]
         },
         {
             salon: "Salon 2",
             events: [
-                { time: "15:00 pm - 16:30 pm", speaker: "Elif Kaya", image: "img/speakers/merve-ucarkosar.jpg", title: "Vue.js Workshop" },
-                { time: "17:00 pm - 18:00 pm", speaker: "Burak Demir", image: "img/speakers/lemi-orhan-ergin.jpg", title: "React Best Practices" }
+                { time: "15:00 pm", speaker: "Elif Kaya", image: "img/speakers/merve-ucarkosar.jpg", title: "Vue.js Workshop" },
+                { time: "17:00 pm", speaker: "Burak Demir", image: "img/speakers/lemi-orhan-ergin.jpg", title: "React Best Practices" }
             ]
         }
     ];
@@ -24,7 +24,7 @@ document.addEventListener("DOMContentLoaded", function () {
         salonElement.style.marginBottom = "32px";
 
         salonElement.innerHTML = `
-            <div class="offset-md-3 col-md-9">
+            <div class="col-md-10">
                 <h4>${salon.salon}</h4>
                 <div class="title-page-line mt-4 mb-3"></div>
             </div>
@@ -38,10 +38,10 @@ document.addEventListener("DOMContentLoaded", function () {
             eventElement.style.marginBottom = "16px";
 
             eventElement.innerHTML = `
-                <div class="col-md-3 schedule-time">
+                <div class="col-md-2 schedule-time">
                     <p>${event.time}</p>
                 </div>
-                <div class="col-md-9 mt-4 mt-md-0" style="display: flex; align-items: center;">
+                <div class="col-md-10 mt-4 mt-md-0" style="display: flex; align-items: center;">
                     <img width="50" height="50" style="border-radius: 50%; margin-right: 10px;" src="${event.image}" alt="${event.speaker}">
                     <div>
                         <h5>${event.title}</h5>
